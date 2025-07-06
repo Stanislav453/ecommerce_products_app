d71e691f977b080be055fe1d59ed8ffd9e7df838
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-06 21:27:31 +0200

    add react-router basicli to app

---
+++ src/Route/Pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router";
+
+export const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      404 Not Found
+      <Link to="/">Home from Link</Link>
+    </div>
+  );
+};
---
+++ src/Route/router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import {HomePage} from "./Pages/HomePage";
+import {NotFoundPage} from "./Pages/NotFoundPage";
+import { Navigation } from "../Components/Navigation/Navigation";
+
+
+export const router = createBrowserRouter([
+  {
+    element: <Navigation />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+        errorElement: <NotFoundPage />,
+      },
+     
+    ],
+  },
+]);
