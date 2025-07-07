4b0365ae03901e01108ec7c2634c34ed7d46585d
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-07 21:38:11 +0200

    Delete unUsed file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
-import { App } from "./App.tsx";
 import { router } from "./Route/router.tsx";
 
 createRoot(document.getElementById("root")!).render(
