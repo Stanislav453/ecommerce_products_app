2c0483f440e0b5aaf08c58332b5a183ed77fe90e
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-07 21:40:17 +0200

    Add Incons like button

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,24 +1,50 @@
 import React from "react";
 import { Link, NavLink, Outlet } from "react-router";
+import {
+  IoSearchOutline,
+  IoPersonOutline,
+  IoBagOutline,
+} from "react-icons/io5";
 
 export const Navigation = () => {
   return (
     <>
-      <div>
-        <a href="/">Glowzy</a>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Shop">Shop</NavLink>
-            </li>
-            <li>
-              <NavLink to="/Blog">Blog</NavLink>
-            </li>
-          </ul>
-        </nav>
+      <div className="flex flex-col items-center">
+        <div className=" w-full max-w-screen-lg flex justify-between py-5">
+          <NavLink className="font-bold text-2xl" to="/">
+            Glowzy.
+          </NavLink>
+          <nav className="flex gap-5">
+            <ul className="flex gap-3">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Shop">Shop</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Blog">Blog</NavLink>
+              </li>
+            </ul>
+            <ul className="flex gap-3">
+              <li>
+                <button>
+                  <IoSearchOutline />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <IoPersonOutline />
+                </button>
+              </li>
+              <li>
+                <button>
+                  <IoBagOutline />
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
       <Outlet />
     </>
