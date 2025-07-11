d434ce2a4bba8b09303ef349d942f12d5a87d2d5
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-11 18:55:02 +0200

    type component

--- src/Store/useProducts.tsx
+++ src/Store/useProducts.tsx
@@ -1,6 +1,12 @@
 import { create } from "zustand";
+import type { ProductType } from "../type";
 
-export const useProducts = create((set) => ({
+interface useProductsType {
+  products: ProductType[];
+  setProducts: (products: ProductType[]) => void;
+}
+
+export const useProducts = create<useProductsType>((set) => ({
   products: [],
-  setProducts: (products: any) => set(() => ({ products: products })),
+  setProducts: (products: ProductType[]) => set(() => ({ products: products })),
 }));
