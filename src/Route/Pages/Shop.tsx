d9af5760171640f9ae1324d4f69adc29fc70bd3d
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-11 18:59:28 +0200

    render data from  api

--- src/Route/Pages/Shop.tsx
+++ src/Route/Pages/Shop.tsx
@@ -1,18 +1,23 @@
 // import { UseFetchData } from "../../api/ApiActions/FetchProducts";
 
 import { useProducts } from "../../Store/useProducts";
+import type { ProductType } from "../../type";
 
 export const Shop = () => {
   const products = useProducts((state: any) => state.products);
-
 
   if (!products) return "Loading...";
 
   return (
     <div>
-      {/* {shopList.products.map((product, index) => {
-        return <h3>{product.title}</h3>;
-      })} */}
+      {products.map((product: ProductType, index: number) => {
+        return (
+          <>
+            <h3>{product.title}</h3>
+            <img src={product.thumbnail} alt={product.title} />
+          </>
+        );
+      })}
     </div>
   );
 };
