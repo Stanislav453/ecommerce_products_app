e9db40d88ab54afcca961a97bffd58ea571fd187
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-11 18:54:33 +0200

    delete unUsed code

--- src/Route/Pages/HomePage.tsx
+++ src/Route/Pages/HomePage.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect } from "react";
 import { Header } from "../../Components/Header/Header";
-import { FetchProducts } from "../../api/ApiActions/FetchProducts";
 import { useProducts } from "../../Store/useProducts";
 
 export const HomePage = () => {
-  const products = useProducts((state: any) => state.products);
 
 
   return (
