2c5a2586af85a7a484422db7f16e36a1ad862882
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-12 13:28:47 +0200

    refactoring if for empty products array

--- src/Route/Pages/Shop.tsx
+++ src/Route/Pages/Shop.tsx
@@ -6,22 +6,18 @@ import type { ProductType } from "../../type";
 export const Shop = () => {
   const products = useProducts((state: any) => state.products);
 
-  // if (!products) return "Loading...";
+  if (products.length == 0) return <div>Loading...</div>;
 
-  const getShopList = () => {
-    if (products.length != 0) {
-      return products.map((product: ProductType, index: number) => {
+  return (
+    <div>
+      {products.map((product: ProductType, index: number) => {
         return (
           <>
             <h3>{product.title}</h3>
             <img src={product.thumbnail} alt={product.title} />
           </>
         );
-      });
-    } else {
-      return <div>Loading...</div>;
-    }
-  };
-
-  return <div>{getShopList()}</div>;
+      })}
+    </div>
+  );
 };
