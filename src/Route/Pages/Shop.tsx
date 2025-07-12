de555cb68a81a4fceab40c3cf2cbf95110b5863f
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-12 15:35:17 +0200

    add PageSection

--- src/Route/Pages/Shop.tsx
+++ src/Route/Pages/Shop.tsx
@@ -5,6 +5,7 @@ import loadingSpinner from "/public/loadingSpinner.svg";
 import type { ProductType } from "../../type";
 import { useError } from "../../Store/useError";
 import { NavLink } from "react-router-dom";
+import { PageSection } from "../../Components/PageSection";
 
 export const Shop = () => {
   const products = useProducts((state: any) => state.products);
@@ -33,25 +34,31 @@ export const Shop = () => {
     );
 
   return (
-    <ul className="flex flex-wrap gap-5 justify-center">
-      {products.map((product: ProductType, index: number) => {
-        const { title, thumbnail, price, rating } = product;
-        return (
-          <li className="bg-linear-to-r from-cyan-500 to-blue-500" key={index}>
-            <div>
-              <img src={thumbnail} alt={title} />
-            </div>
-            <div className="flex flex-col gap-1 text-center">
-              <h3 className="font-semibold text-xl">{title}</h3>
-              <p className="font-semibold text-xl">${price}</p>
-              <p className="font-semibold text-xl"> {rating} </p>
-              <button className="w-full  py-2 border-2 border-black		rounded-full">
-                Add to card
-              </button>
-            </div>
-          </li>
-        );
-      })}
-    </ul>
+    <section>
+      <PageSection>Shop</PageSection>
+      <ul className="flex flex-wrap gap-5 justify-center">
+        {products.map((product: ProductType, index: number) => {
+          const { title, thumbnail, price, rating } = product;
+          return (
+            <li
+              className="bg-linear-to-r from-cyan-500 to-blue-500"
+              key={index}
+            >
+              <div>
+                <img src={thumbnail} alt={title} />
+              </div>
+              <div className="flex flex-col gap-1 text-center">
+                <h3 className="font-semibold text-xl">{title}</h3>
+                <p className="font-semibold text-xl">${price}</p>
+                <p className="font-semibold text-xl"> {rating} </p>
+                <button className="w-full  py-2 border-2 border-black		rounded-full">
+                  Add to card
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 };
