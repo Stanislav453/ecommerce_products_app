296c81b2126bcd6a8778f1b4f5582925f3db2ddc
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-13 19:41:29 +0200

    create ShopItems

---
+++ src/Components/Shop/ShopItems.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import type { ProductType } from "../../type";
+
+interface shopListType {
+  shopList: ProductType[];
+}
+
+export const ShopItems = ({ shopList }: shopListType) => {
+  return (
+    <ul className="flex flex-wrap gap-5 justify-center">
+      {shopList.map((product: ProductType, index: number) => {
+        const { title, thumbnail, price, rating } = product;
+        return (
+          <li className="bg-linear-to-r from-cyan-500 to-blue-500" key={index}>
+            <div>
+              <img src={thumbnail} alt={title} />
+            </div>
+            <div className="flex flex-col gap-1 text-center">
+              <h3 className="font-semibold text-xl">{title}</h3>
+              <p className="font-semibold text-xl">${price}</p>
+              <p className="font-semibold text-xl"> {rating} </p>
+              <button className="w-full  py-2 border-2 border-black		rounded-full">
+                Add to card
+              </button>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
