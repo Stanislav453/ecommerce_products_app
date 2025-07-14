9d09c4eebd1db0ccd00ef6384c1d184db5356481
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-14 20:48:50 +0200

    create shopFilter

--- src/Components/Shop/ShopFilter.tsx
+++ src/Components/Shop/ShopFilter.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { useProducts } from "../../Store/useProducts";
+import type { ProductType } from "../../type";
 
-export const ShopFilter = () => {
+interface shopFilterType {
+  setShopList: (product: ProductType) => void;
+}
+
+export const ShopFilter = ({ setShopList }: shopFilterType) => {
   const products = useProducts((state: any) => state.products);
 
-  const shopList =
-    categorySelect == shopListCategory[0]
-      ? products
-      : products.filter((product) => product.category == categorySelect);
+  const shopListCategory = [
+    "All",
+    ...new Set(products.map((product: any) => product.category)),
+  ];
 
   const [categorySelect, setCategorySelect] = useState(shopListCategory[0]);
+
+  const categorySelectHandler = (event: any) => {
+    setCategorySelect(event.target.value);
+  };
+
+  const filterList =
+    categorySelect == shopListCategory[0]
+      ? products
+      : products.filter((product: any) => product.category == categorySelect);
+
+  useEffect(() => {
+    setShopList(filterList);
+  }, [categorySelect]);
 
   return (
     <select name="category-filter" onChange={categorySelectHandler}>
