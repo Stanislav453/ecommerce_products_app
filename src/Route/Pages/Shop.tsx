ecd66c010acc2e78475c3cbeb8c04bd7fc6755ec
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-07-30 14:04:58 +0200

    detele fetch products and loading

--- src/Route/Pages/Shop.tsx
+++ src/Route/Pages/Shop.tsx
@@ -1,32 +1,16 @@
-import loadingSpinner from "/public/loadingSpinner.svg";
 import { NavLink } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ShopItems } from "../../Components/Shop/ShopItems";
 import { PageSection } from "../../Components/PageSection";
-import { UseFetchData } from "../../api/ApiActions/UseFetchData";
-import type { ProductSummary, ProductSummaryResponse } from "../../type";
+import type { ProductSummary } from "../../type";
 import { ShopFilter } from "../../Components/Shop/ShopFilter";
 
 export const Shop = () => {
-  const { data: response, loading } = UseFetchData<ProductSummaryResponse>(
-    "/products?select=id,title,thumbnail,price,rating,description"
-  );
   const [shopList, setShopList] = useState<ProductSummary[]>([]);
 
-  useEffect(() => {
-    if (response) {
-      setShopList(response.products);
-    }
-  }, [response]);
 
-  if (loading)
-    return (
-      <div className="flex w-full justify-center mt-28">
-        <img className="w-12" src={loadingSpinner} alt="loading" />
-      </div>
-    );
 
-  if (response == null)
+  if (shopList == null)
     return (
       <div className="flex flex-col w-full items-center mt-28">
         <p className="text-red-600">Something is wrong.</p>
