07470873bc568c3b932fb4a98976ffbda3688f90
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-09-06 15:03:35 +0200

    create fetch variant for  useFetch

---
+++ src/fetchArgs.ts
@@ -0,0 +1,21 @@
+import { ArgsOptions, Category } from "./type";
+
+type FetchArgsProps = {
+  selectedValue: Category;
+};
+
+export const fetchArgs = ({ selectedValue }: FetchArgsProps) => {
+  const args: ArgsOptions =
+    selectedValue === Category.All
+      ? {
+          kind: "products",
+          query: "?select=id,title,thumbnail,price,rating,description",
+        }
+      : {
+          kind: "category",
+          name: selectedValue,
+          query: "?select=id,title,thumbnail,price,rating,description",
+        };
+
+  return { args };
+};
