30e89ed89e53627b6ce61a8243fb6c4a51241683
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-09-13 10:01:22 +0200

    Fix responsivity

--- src/Components/ProductDetail/ProdDescContainer.tsx
+++ src/Components/ProductDetail/ProdDescContainer.tsx
@@ -19,8 +19,8 @@ export const ProdDescContainer = ({
     value === buttonValue ? "bg-des-nav-color" : "bg-white border-b-[1px] border-theme-gray-border";
 
   return (
-    <article className="w-full max-w-screen-xl flex  gap-12 justify-between items-start py-5 px-3 ">
-      <ul className=" w-72">
+    <article className="w-full max-w-screen-xl flex flex-col md:flex-row gap-12 justify-between items-start py-5 px-3 ">
+      <ul className=" w-full md:w-72">
         <li>
           <button
             className={`w-full text-left text-lg font-medium py-4 px-2 ${getButtonClass(
