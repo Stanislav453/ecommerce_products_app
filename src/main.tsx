8e7af8eafee92a40cd19e7cfa69135b02bd1ba95
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-09-28 18:23:42 +0200

    add tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,20 @@ import { RouterProvider } from "react-router";
 import "./index.css";
 import { router } from "./Route/router.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
+
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: import("@tanstack/query-core").QueryClient;
+  }
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
