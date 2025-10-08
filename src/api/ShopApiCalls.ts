3f340dfccd1538587c1195ed2df936f05cb4d52d
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-10-08 19:56:35 +0200

    add new fetch function

--- src/api/ShopApiCalls.ts
+++ src/api/ShopApiCalls.ts
@@ -1,10 +1,20 @@
 import axios from "axios";
-import { ProductSummaryResponse } from "../type";
+import { ProductDetailResponse, ProductSummaryResponse } from "../type";
 
 export const getSummuryProducts = async (): Promise<ProductSummaryResponse> => {
   return (
     await axios.get(
       "https://dummyjson.com/products?select=title,price,rating,thumbnail"
+    )
+  ).data;
+};
+
+export const getProductDetail = async (
+  id: string | null
+): Promise<ProductDetailResponse> => {
+  return (
+    await axios.get(
+      `https://dummyjson.com/products/${id}?select=id,title,images,price,rating,description,category,tags,reviews`
     )
   ).data;
 };
