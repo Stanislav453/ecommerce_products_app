1a707b6d787c61ec91c905b8f93e5264268025a7
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-10-10 19:28:48 +0200

    remove unused code

--- src/Components/Shop/ShopContainer.tsx
+++ src/Components/Shop/ShopContainer.tsx
@@ -10,7 +10,7 @@ import { Category } from "../../type";
 export const ShopContainer = () => {
   const [selectFilterValue, setselectedValue] = useState(Category.All);
 
-  const { data, error, isLoading, isFetching, refetch } =
+  const { data, error, isLoading, isFetching } =
     shopRepository.shopProductsSummury.useQuery(selectFilterValue);
 
   if (error) return <ApiCallError />;
@@ -25,7 +25,6 @@ export const ShopContainer = () => {
     <section>
       <PageSection>
         <h1 className="font-bold text-3xl">Shop</h1>
-        <button onClick={() => refetch()}>Refetch</button>
       </PageSection>
       <div className="flex flex-col items-center py-4 sm:py-0 sm:pb-4 sm:pt-20">
         <div className="w-full  max-w-screen-xl flex flex-col sm:flex-row  gap-12 sm:gap-0 justify-center sm:justify-between items-center px-3">
