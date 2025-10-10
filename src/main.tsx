4df215d18a43b061696d5c44e5890aa5a3955360
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-10-10 19:38:49 +0200

    preperty tanstack globaly

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,16 @@ import { router } from "./Route/router.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: 1,
+      staleTime: 10 * 60 * 1000,
+    },
+  },
+});
 
 declare global {
   interface Window {
