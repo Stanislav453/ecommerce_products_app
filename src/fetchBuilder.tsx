06460e206c53d0365900db361edc123b49d0fcc0
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-11-18 11:11:15 +0100

    refactoring

--- src/fetchBuilder.tsx
+++ src/fetchBuilder.tsx
@@ -5,9 +5,6 @@ import { fetchpath } from "./fetchPath";
 export const fetchBuilder = async <T extends {}>(options: FetchOptions) => {
   const { path } = fetchpath(options);
 
-  console.log("This is path from FETCHBUILDER",path);
-  
-
   const response = await axios.request<T>({
     method: options.method,
     url: path,
