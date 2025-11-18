da592618d1293632bc1a516f82563ce501c3c005
Stanislav Jendrisak <st.jendrisak@gmail.com>
2025-11-18 10:06:02 +0100

    delete unUsed code

--- src/Route/Pages/HomePage.tsx
+++ src/Route/Pages/HomePage.tsx
@@ -1,8 +1,7 @@
 import { Header } from "../../Components/Header/Header";
+import { fetchBuilder } from "../../fetchBuilder";
 
 export const HomePage = () => {
-
-
   return (
     <div>
       <Header />
