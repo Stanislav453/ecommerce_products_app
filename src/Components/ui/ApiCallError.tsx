c8025c353728b9777e57199ca80fddb636e1ab1f
Jiri Pohanka <jiri.pohanka@mews.com>
2025-12-04 12:30:22 +0100

    made some sample changes during consult

--- src/Components/ui/ApiCallError.tsx
+++ src/Components/ui/ApiCallError.tsx
@@ -1,19 +1,20 @@
 import { AxiosError } from "axios";
-import React from "react";
 import { NavLink } from "react-router-dom";
 
 type ApiCallErrorProps = {
-  error: Error | AxiosError | null;
+  error: Error | AxiosError | string;
 };
 
 export const ApiCallError = ({ error }: ApiCallErrorProps) => {
-  if (error) {
-    throw error;
+
+  if(error instanceof AxiosError || error instanceof Error)  {
+    // do something, inform it's failed fetch, just an example of error handling logic here
+    return null;
   }
 
   return (
     <div className="flex flex-col w-full items-center mt-28">
-      <p className="text-red-600">Something is wrong.</p>
+      <p className="text-red-600">Something is wrong. {error}</p>
       <p>
         Please go to
         <NavLink to="/" className="font-bold underline	">
