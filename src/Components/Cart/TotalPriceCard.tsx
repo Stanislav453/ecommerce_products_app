266e7371e7ec7f717de528b52792778c33170ab8
Stanislav Jendrisak <st.jendrisak@gmail.com>
2026-01-04 18:01:34 +0100

    delete unused code

--- src/Components/Cart/TotalPriceCard.tsx
+++ src/Components/Cart/TotalPriceCard.tsx
@@ -10,14 +10,6 @@ const TotalPriceCard = ({ subTotal }: TotalPriceCardProps) => {
         <p className="text-2xl font-semibold text-gray-600">${subTotal}</p>
       </div>
       <p className="my-3">Shipping and discounts calculated at checkout.</p>
-      <div className="flex  gap-4">
-        <button className=" text-xl border-2 border-black p-2 rounded-xl">
-          View my cart
-        </button>
-        <button className=" text-xl text-white p-2 border-2 rounded-xl bg-black">
-          Go to checkout
-        </button>
-      </div>
     </div>
   );
 };
